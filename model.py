880172ba00d770c786e213a0db678af014f2e240
jtzhangLogan <jzhan247@jhu.edu>
2021-06-29 20:31:38 -0400

    added encoder-decoder transformer model

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import copy
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from encoder import Encoder
 import math
 
 
@@ -51,7 +52,8 @@ class MultiHeadSelfAttention(nn.Module):
         self.linear = nn.Linear(d_model, d_model)
 
     def forward(self, q, k, v, mask):
-        assert q.shape[1] == k.shape[1] and k.shape[1] == v.shape[1], "self-attention input doesn't have equal length"
+        if torch.equal(q, k) and torch.equal(k, v):
+            assert q.shape[1] == k.shape[1] and k.shape[1] == v.shape[1], "self-attention input doesn't have equal length"
         nbatch = q.shape[0]
         _q = self.w_q(q).view(nbatch, -1, self.nhead, self.d_k).transpose(1, 2)
         _k = self.w_k(k).view(nbatch, -1, self.nhead, self.d_k).transpose(1, 2)
@@ -83,7 +85,10 @@ class PositionalEncoding(nn.Module):
 
 class DecoderLayer(nn.Module):
     """
-    Transformer Decoder Layer
+    Transformer Decoder Layer:
+    Applies self-attention on embedded kinematics sequences, followed by cross-attention on encoded video captures
+    :param x: embedded kinematics sequences
+    :param y: encoded video captures
     """
 
     def __init__(self, interm_dim: int = 2048, feat_dim: int = 512, nhead: int = 8, dropout: int = 0, device: str = 'cuda', use_norm: bool = False, is_mask_enable: bool = False):
@@ -95,6 +100,7 @@ class DecoderLayer(nn.Module):
 
         self.norm1 = nn.LayerNorm(feat_dim)
         self.norm2 = nn.LayerNorm(feat_dim)
+        self.norm3 = nn.LayerNorm(feat_dim)
 
         self.dropout = nn.Dropout(dropout)
         self.activation = nn.ReLU()
@@ -103,24 +109,30 @@ class DecoderLayer(nn.Module):
         self.use_norm = use_norm
         self.is_mask_enable = is_mask_enable
 
-    def forward(self, x):
-        # x.shape: N * L * D
+    def forward(self, x, y):
         # generate mask
         mask = generate_attn_mask(x.size(1)).to(self.device) if self.is_mask_enable else None
         # self-attn
         x = self.attn_layer(q=x, k=x, v=x, mask=mask) + x
+        if self.use_norm:
+            x = self.norm1(x)
+        # cross-attn
+        x = self.attn_layer(q=x, k=y, v=y, mask=None) + x
         # feed forward
         if self.use_norm:
-            x = self.norm1(x)
+            x = self.norm2(x)
         x = self.linear2(self.activation(self.linear1(x))) + x
         if self.use_norm:
-            x = self.norm1(x)
+            x = self.norm3(x)
         return x
 
 
 class Decoder(nn.Module):
     """
-    Transformer Decoder
+    Transformer Decoder:
+    Stacked collections of decoder layers
+    :param x: embedded kinematics sequences
+    :param y: encoded video captures
     """
 
     def __init__(self, interm_dim: int = 2048, feat_dim: int = 256, nhead: int = 8, num_attn_layers: int = 10,
@@ -132,17 +144,15 @@ class Decoder(nn.Module):
                                           device=device, use_norm=use_norm, is_mask_enable=is_mask_enable)
         self.decoders = get_clones(self.decoder_layer, num_attn_layers)
 
-    def forward(self, x):
+    def forward(self, x, y):
         """
-
-        :param x: CNN feature map, extract from input kinematics sequence [W, N, C1]
-        :param y: raw input kinematics sequence [W, N, C2]
-        :return:
+        :param x: embedded kinematics sequences
+        :param y: encoded video captures
         """
         x = self.pos_encoder.forward(x)
 
         for module in self.decoders:
-            x = module(x)
+            x = module(x, y)
 
         return x
 
@@ -152,9 +162,7 @@ class Classifier(nn.Module):
         super().__init__()
         # bs * (sl * dim) --> bs * (ousl * dim2)
         self.classifier = nn.Sequential(
-            nn.Linear(in_channels, in_channels // 32),
-            nn.ReLU(),
-            nn.Linear(in_channels // 32, in_channels // 512),
+            nn.Linear(in_channels, in_channels // 512),
             nn.ReLU(),
             nn.Linear(in_channels // 512, out_channels)
         )
@@ -210,8 +218,61 @@ class DecoderOnlyTransformer(nn.Module):
         return output
 
 
+class EncoderDecoderTransformer(nn.Module):
+    def __init__(self, feat_dim, nhead, num_attn_layers, channel, device, use_norm, is_mask_enable, input_frames,
+                 output_frames, is_feature_extract, num_conv_layers, input_channel, output_channel, conv_kernel_size,
+                 conv_stride, pool_kernel_size, pool_stride, padding, img_height, img_width, patch_height, patch_width,
+                 in_dim, batch_size, capture_size, dropout):
+        super().__init__()
+        self.feat_dim = feat_dim
+        self.nhead = nhead
+        self.num_attn_layers = num_attn_layers
+        self.channel = channel
+        self.input_frames = input_frames
+        self.output_frames = output_frames
+        self.batch_size = batch_size
+        self.is_mask_enable = is_mask_enable
+
+        self.encoder = Encoder(is_feature_extract=is_feature_extract, num_conv_layers=num_conv_layers, input_channel=input_channel,
+                    output_channel=output_channel, conv_kernel_size=conv_kernel_size, conv_stride=conv_stride, pool_kernel_size=pool_kernel_size,
+                    pool_stride=pool_stride, padding=padding, img_height=img_height, img_width=img_width, patch_height=patch_height,
+                    patch_width=patch_width, in_dim=in_dim, feat_dim=feat_dim, batch_size=batch_size, capture_size=capture_size, dropout=dropout, device=device)
+
+        self.embeddings = nn.Linear(channel, feat_dim)
+        self.decoder = Decoder(interm_dim=2048, feat_dim=feat_dim, nhead=nhead, num_attn_layers=num_attn_layers,
+                               dropout=0, device=device, use_norm=use_norm, is_mask_enable=is_mask_enable)
+        self.final = nn.Linear(feat_dim, channel)
+        self.final_classifier = Classifier(in_channels=feat_dim * input_frames, out_channels=channel * output_frames)
+
+    def forward(self, input):
+        """
+        :param psm1_past: feature descriptor of past kinematics, [N,C,W]
+        :param psm1_future: feature descriptor of future kinematics, [N,C,W]
+        :param pos_enc: relative positional encoding, [N,C,2W-1]
+        :return: cross attention values [N,H,W,W], dim=2 is left image, dim=3 is right image
+        """
+        # encoder video captures
+        encoded_cap = self.encoder(input["captures"]) # encoded capture shape [N, capture_size, patch_size, dimension]
+        # print("encoding shape = {}".format(encoded_cap.shape))
+
+        # kinematics embeddings
+        embedded_kin = self.embeddings(input["kinematics"].permute(0, 2, 1))
+        # print("embedding shape = {}".format(embedded_kin.shape))
+
+        # Decode
+        decoder_output = self.decoder(embedded_kin, encoded_cap.reshape(self.batch_size, -1, self.feat_dim))
+
+        # Final layer
+        if self.is_mask_enable:
+            output = self.final(decoder_output).permute(0, 2, 1)  # input shape [N, W, C]
+        else:
+            output = self.final_classifier(decoder_output.reshape(-1, self.feat_dim * self.input_frames)).reshape(-1, self.output_frames, self.channel).permute(0, 2, 1)
+        # print("fully connected layers output = {}".format(output.shape))
+
+        return output
+
+
 if __name__ == "__main__":
-    print(generate_attn_mask(10))
     m = nn.Softmax(dim=1)
     input = torch.randn(2, 3)
     output = m(input)
